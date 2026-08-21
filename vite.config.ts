c38fe6f086e0fe6e3902527b8807c82013458ea4
Prasad Pilke <prasadpilke21@gmail.com>
2026-08-21 19:32:57 +0530

    Fix Netlify Functions directory, static publishing, and routing rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ export default defineConfig({
   plugins: [
     build({
       entry: 'src/index.tsx',
+      outputDir: 'netlify/functions'
     }),
     devServer({
       entry: 'src/index.tsx'
     })
-  ]
+  ],
+  build: {
+    copyPublicDir: false
+  }
 })
